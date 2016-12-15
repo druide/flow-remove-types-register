5ff70f164e4df352b7871db08d01a32a46428590
druide <ingvuil@gmail.com>
2016-12-15 18:38:50 +0200

    [Fix] Flow require hook update.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # flow-remove-types-register
 
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![Flowtype](https://img.shields.io/badge/type%20check-flow-brightgreen.svg)](https://flowtype.org/)
+[![npm version](https://img.shields.io/npm/v/@druide/flow-remove-types-register.svg)](https://www.npmjs.com/package/@druide/flow-remove-types-register)
+[![license](https://img.shields.io/github/license/druide/flow-remove-types-register.svg)](https://github.com/druide/flow-remove-types-register)
+
 Add ability to `require()` [Flow](https://flowtype.org) modules and files using
 [flow-remove-types](https://github.com/leebyron/flow-remove-types).
 
@@ -11,10 +16,16 @@ npm i @druide/flow-remove-types-register --save
 
 ## Usage
 
-Require this module at your program or module entry point.
+Require this module at your program entry point (index.js):
 
 ```
-require('@druide/flow-remove-types-register')
+require('@druide/flow-remove-types-register')()
+```
+
+or at your module entry point:
+
+```
+require('@druide/flow-remove-types-register')({module: 'moduleName'})
 ```
 
 Write you code with Flow:
@@ -29,6 +40,9 @@ Flow cannot be used at program or module entry point directly.
 Declare your `index.js` this way:
 
 ```
-require('@druide/flow-remove-types-register')
+require('@druide/flow-remove-types-register')()
 require('./my-app-with-flow')
 ```
+
+Also notice some program startup time slow down due to source files
+pre-processing.
--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-var Module = require('module')
-var removeTypes = require('flow-remove-types')
-var path = require('path')
+var flowRemoveTypes = require('flow-remove-types')
 
-// Rather than use require.extensions, swizzle Module#_compile. Not only does
-// this typically leverage the existing behavior of require.extensions['.js'],
-// but allows for use alongside other "require extension hook" if necessary.
-var superCompile = Module.prototype._compile
-Module.prototype._compile = function _compile (source, filename) {
-  var transformedSource = path.extname(filename) === '.js'
-    ? removeTypes(source)
-    : source
-  superCompile.call(this, transformedSource, filename)
+// Supported options:
+//
+//   - all: Transform all files, not just those with a @flow comment.
+//   - includes: A Regexp to determine which files should be transformed.
+//   - excludes: A Regexp to determine which files should not be transformed,
+//               defaults to ignoring /node_modules/, set to null to excludes nothing.
+//   - module: Module name to exclude from excludes :). This option is cumulative:
+//             setting option `module` will add the name to the previous registered
+//             names.
+var options
+var flowModules = []
+module.exports = function setOptions (newOptions) {
+  options = newOptions
+  if (options.module) flowModules.push(options.module)
+}
+
+// Swizzle Module#_compile on each applicable module instance.
+// NOTE: if using alongside Babel or another require-hook which simply
+// over-writes the require.extensions and does not continue execution, then
+// this require hook must come after it. Encourage those module authors to call
+// the prior loader in their require hooks.
+var jsLoader = require.extensions['.js']
+console.log(require)
+var exts = [ '.js', '.jsx', '.flow', '.es6' ]
+exts.forEach(function (ext) {
+  var superLoader = require.extensions[ext] || jsLoader
+  require.extensions[ext] = function (module, filename) {
+    if (shouldTransform(filename.replace(/\\/g, '/'), options)) {
+      var superCompile = module._compile
+      module._compile = function _compile (code, filename) {
+        superCompile.call(this, flowRemoveTypes(code, options).toString(), filename)
+      }
+    }
+    superLoader(module, filename)
+  }
+})
+
+function shouldTransform (filename, options) {
+  var includes = options && options.includes
+  var excludes = options && 'excludes' in options ? options.excludes : /\/node_modules\//
+  return (!includes || includes.test(filename)) &&
+    !(excludes && excludes.test(filename) && !isFlowModule(filename))
+}
+
+function isFlowModule (filename) {
+  for (var i = 0, len = flowModules.length; i < len; i++) {
+    if (filename.indexOf('node_modules/' + flowModules[i] + '/') !== -1) return true
+  }
+  return false
 }
